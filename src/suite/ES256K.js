98f9efe2cfdd0e53df628e91061e2df2f66d2e42
Oleksandr Kravets <a@kra.vc>
2021-03-02 21:08:13 +0200

    Tweak ES256K workarounds for latest transmute updates

--- src/suite/ES256K.js
+++ src/suite/ES256K.js
@@ -4,35 +4,25 @@ const alg  = 'ES256K'
 const type = 'EcdsaSecp256k1VerificationKey2019'
 
 const {
-  ES256K:   { sign, decode, verify, signDetached, verifyDetached: _verifyDetached },
+  ES256K:   { sign, decode, verify: _verify, signDetached, verifyDetached },
   driver:   { resolve },
-  keyUtils: { publicKeyJwkFromPublicKeyHex, publicKeyUInt8ArrayFromPublicKeyBase58 }
+  keyUtils: { publicKeyJwkFromPublicKeyBase58, publicKeyUInt8ArrayFromPublicKeyBase58 }
 } = require('@transmute/did-key-secp256k1')
 
 const { Secp256k1KeyPair: KeyPair } = require('@transmute/did-key-secp256k1')
 
-const publicKeyJwkFromPublicKeyBase58 = value => {
-  const publicKeyHex = publicKeyUInt8ArrayFromPublicKeyBase58(value).toString('hex')
-  const publicKeyJwk = publicKeyJwkFromPublicKeyHex(publicKeyHex)
+// TODO: https://github.com/transmute-industries/did-key.js/issues/63
+const verify = async (...args) => {
+  const isVerified = await _verify(...args)
 
-  return publicKeyJwk
-}
-
-const verifyDetached = async (jws, credentialDigestBuffer, publicKeyJwk) => {
-  try {
-    await _verifyDetached(jws, credentialDigestBuffer, publicKeyJwk)
-
-  } catch (error) {
-    const isVerificationFailed = error.message.includes('ECDSA Verify Failed')
-
-    if (isVerificationFailed) {
-      return false
-    }
-
-    throw error
+  /* istanbul ignore next: to be resolved when new version of crypto suite is released */
+  if (!isVerified) {
+    throw Error('Token verification failed')
   }
 
-  return true
+  const { payload } = await decode(args[0], { complete: true })
+
+  return payload
 }
 
 module.exports = {
